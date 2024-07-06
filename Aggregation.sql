f20787f12c744bf526300e5488a7a34d63e76dde
Shabnaj L <shabnajliaqat@gmail.com>
2024-07-07 00:04:57 +0600

    Update Aggregation.sql

--- Aggregation.sql
+++ Aggregation.sql
@@ -3,3 +3,8 @@ SELECT SUM(POPULATION) FROM CITY
 WHERE DISTRICT = 'California';
 
 # -------------------------------------------
+## Revising Aggregations - The CountFunction
+SELECT COUNT(NAME) FROM CITY
+WHERE POPULATION > 100000;
+
+# -------------------------------------------
