8f82b070d424ba8b7aa240fae8a59547a640fe12
Shabnaj L. <shabnajliaqat@gmail.com>
2024-10-18 19:59:32 +0600

    Update Java-introductrory.java

--- Java-introductrory.java
+++ Java-introductrory.java
@@ -137,6 +137,34 @@ public class Solution {
 }
 
 
+//===============================================================
+//Java loops II
+
+import java.util.*;
+import java.io.*;
+
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int s = a + 1*b;
+            System.out.print(Integer.toString(s)+' ');
+            for(int j=1;j<n;j++){
+                double c = Math.pow(2,j);
+                s = s +(int) c*b;
+                System.out.print(Integer.toString(s)+' ');
+            }System.out.println();
+        }
+        in.close();
+    }
+}
+
+
 
 
 
